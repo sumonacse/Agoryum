1c98ab806229e42645b237c996b50ff1389f4703
Sifat Kazi <75115364+SIFAT69@users.noreply.github.com>
2021-05-15 18:19:51 +0600

    0.3v

--- app/Http/Controllers/BannerSectionController.php
+++ app/Http/Controllers/BannerSectionController.php
@@ -6,25 +6,60 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use Auth;
+use Image;
 
 class BannerSectionController extends Controller
 {
     public function index()
     {
-      return view('Dashboard.Section.banner');
+      $Banner = DB::table('banner_sections')->where('id', 1)->first();
+      return view('Dashboard.Section.banner',compact('Banner'));
     }
 
     public function BannerSection(Request $request)
     {
-      DB::table('banner_sections')->insert([
-        'bannerSectionOne' => $request->bannerTitleOne,
-        'bannerSectionTwo' => $request->bannerTitleTwo,
-        'bannerSectionThree' => $request->bannerTitleThree,
-        'image' => "55",
-        'shortDesc' => $request->bannerDesc,
-        'facebook_link' => $request->facebook_link,
-        'linkedin_link' => $request->linkedin_link,
-        'twitter_link' => $request->twitter_link,
-      ]);
+      $logo_rename = "";
+      $isEmpty = DB::table('banner_sections')->where('id', 1)->first();
+      if (empty($isEmpty)) {
+        if (!empty($request->banner_image)) {
+          $randomNumber =rand();
+          $logo = $request->file('banner_image');
+          $logo_rename = $randomNumber.'.'.$logo->getClientOriginalExtension();
+          $newLocation = 'uploads/'.$logo_rename;
+          Image::make($logo)->save($newLocation,100);
+        }
+        DB::table('banner_sections')->insert([
+          'bannerSectionOne' => $request->bannerTitleOne,
+          'bannerSectionTwo' => $request->bannerTitleTwo,
+          'bannerSectionThree' => $request->bannerTitleThree,
+          'image' => $logo_rename,
+          'shortDesc' => $request->bannerDesc,
+          'facebook_link' => $request->facebook_link,
+          'linkedin_link' => $request->linkedin_link,
+          'twitter_link' => $request->twitter_link,
+        ]);
+      }else {
+        if (!empty($request->banner_image)) {
+          $randomNumber =rand();
+          $logo = $request->file('banner_image');
+          $logo_rename = $randomNumber.'.'.$logo->getClientOriginalExtension();
+          $newLocation = 'uploads/'.$logo_rename;
+          Image::make($logo)->save($newLocation,100);
+        }
+        DB::table('banner_sections')->where('id', 1)->update([
+          'bannerSectionOne' => $request->bannerTitleOne,
+          'bannerSectionTwo' => $request->bannerTitleTwo,
+          'bannerSectionThree' => $request->bannerTitleThree,
+          'image' => $logo_rename,
+          'shortDesc' => $request->bannerDesc,
+          'facebook_link' => $request->facebook_link,
+          'linkedin_link' => $request->linkedin_link,
+          'twitter_link' => $request->twitter_link,
+        ]);
+      }
+
+
+
+      return back()->with('success', 'Banner Updated successfully!');
     }
 }
--- resources/views/Dashboard/Section/banner.blade.php
+++ resources/views/Dashboard/Section/banner.blade.php
@@ -14,6 +14,8 @@
               </div>
           </div>
           <div class="widget-content widget-content-area">
+            @if (empty($Banner))
+
               <form action="{!! route('BannerSectionPost') !!}" method="post" enctype="multipart/form-data">
                 @csrf
                   <div class="form-group mb-3">
@@ -42,6 +44,37 @@
                   </div>
                   <button type="submit" class="btn btn-primary mt-3">Save</button>
               </form>
+              @endif
+              @if (!empty($Banner))
+              <form action="{!! route('BannerSectionPost') !!}" method="post" enctype="multipart/form-data">
+                @csrf
+                  <div class="form-group mb-3">
+                      <input type="text" class="form-control" required name="bannerTitleOne" value="{{ $Banner->bannerSectionOne }}" placeholder="Banner Title 1">
+                  </div>
+                  <div class="form-group mb-3">
+                      <input type="text" class="form-control" required name="bannerTitleTwo" value="{{ $Banner->bannerSectionTwo }}" placeholder="Banner Title 2">
+                  </div>
+                  <div class="form-group mb-3">
+                      <input type="text" class="form-control" required name="bannerTitleThree" value="{{ $Banner->bannerSectionThree }}" placeholder="Banner Title 3">
+                  </div>
+                  <div class="form-group mb-3">
+                      <input type="text" class="form-control" required name="bannerDesc" value="{{ $Banner->shortDesc }}" placeholder="Banner short description">
+                  </div>
+                  <div class="form-group mb-3">
+                      <input type="text" class="form-control" required name="facebook_link" value="{{ $Banner->facebook_link }}" placeholder="facebook link">
+                  </div>
+                  <div class="form-group mb-3">
+                      <input type="text" class="form-control" required name="linkedin_link" value="{{ $Banner->linkedin_link }}" placeholder="linkedin link">
+                  </div>
+                  <div class="form-group mb-3">
+                      <input type="text" class="form-control" required name="twitter_link" value="{{ $Banner->twitter_link }}" placeholder="twitter link">
+                  </div>
+                  <div class="form-group mb-3">
+                      <input type="file" class="form-control-file" required name="banner_image" value="{!! asset('uploads') !!}/{{ $Banner->image }}" placeholder="Banner image">
+                  </div>
+                  <button type="submit" class="btn btn-primary mt-3">Save</button>
+              </form>
+              @endif
           </div>
       </div>
   </div>
